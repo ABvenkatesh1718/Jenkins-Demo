e874182575113c823f758239f53202163a4fb093
ABvenkatesh1718 <126935483+ABvenkatesh1718@users.noreply.github.com>
2025-04-26 03:32:38 +0000

    code updated

--- demo.java
+++ demo.java
@@ -1,8 +1,5 @@
-class HelloWorld {
+public class demo {
     public static void main(String[] args) {
-        System.out.println("Hello Java from Git");
-        int a=10;
-        int b=20;
-        System.out.println("sum is "+(a+b));
+        System.out.println("Hello, World!");
     }
 }
