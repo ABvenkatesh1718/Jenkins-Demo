24e03b34ef16eb7c8989a49b2039b441b5e5e9f2
ABvenkatesh1718 <126935483+ABvenkatesh1718@users.noreply.github.com>
2025-04-26 03:26:35 +0000

    java file created and updated

---
+++ demo.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Java from Git");
+        int a=10;
+        int b=20;
+        System.out.println("sum is "+(a+b));
+    }
+}
