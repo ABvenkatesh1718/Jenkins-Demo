431ce52382ea161c6868cf7e9b303a1dfb2af85d
ABvenkatesh1718 <126935483+ABvenkatesh1718@users.noreply.github.com>
2025-04-26 16:18:03 +0000

    code updated with webhook up

--- demo.java
+++ demo.java
@@ -2,6 +2,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("check new commits will trigger auto builds");
         System.out.println("Java from git with webhook trigger auto");
-        System.out.println("Thank You from Java ");
+        System.out.println("Thank You from Java");
     }
 }
